3896932a2fb0724e9d645e31db7750d208fc26e1
Akash Kumar <Akash.Kumar@ivycomptech.com>
2024-01-14 18:47:53 +0530

    creation of auth tool

--- backend/Routes/LoginUser.js
+++ backend/Routes/LoginUser.js
@@ -2,6 +2,9 @@ const express = require("express");
 const loginRouter = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const jwtSecret = "MYNAMEISAKASHKUMARSHARMAILIVEINDEHRADUN"
 
 loginRouter.post(
   "/loginuser",
@@ -20,10 +23,20 @@ loginRouter.post(
       if (!userData) {
         return res.status(400).json({ error: "Email Id Doesnot exist" });
       }
-      if (req?.body?.password !== userData?.password) {
+      //for comparing crypted data
+      const pwdCompare = await bcrypt.compare(req.body.password,userData.password);
+      console.log(pwdCompare);
+      if (!pwdCompare) {
         return res?.status(400).json({ error: "Enter Correct Password" });
       }
-      return res.json({ success: true });
+      const data = {
+        user:{
+          id: userData.id
+        }
+      }
+      //creating authToken
+      const authToken = jwt.sign(data,jwtSecret);
+      return res.json({ success: true,authToken:authToken });
     } catch (error) {
       console.log(error);
       res.json({ success: false });
